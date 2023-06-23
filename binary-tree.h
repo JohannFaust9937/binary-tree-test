397f0a7c161a68df478d9abe768f119eeeac30dc
JohannFaust9937 <111689269+JohannFaust9937@users.noreply.github.com>
2023-06-23 14:10:38 +0300

    update

--- binary-tree.h
+++ binary-tree.h
@@ -3,23 +3,22 @@
 
 struct Node {
 	int key{ 0 }, value{ 0 };
-	Node*left{ nullptr }, *right{ nullptr };
+	Node* left{ nullptr }, * right{ nullptr };
 
 	Node() = default;
 	Node(int key, int value) : key{ key }, value{ value } {}
-	~Node() {}
+	~Node() {
+		printf("\nRun ~Node()");
+	}
 
-	void insert(int key, int value)
-	{
-		if (key < this->key)
-		{
+	void insert(int key, int value, int depth = 0) {
+		if (key < this->key) {
 			if (left == nullptr)
 				left = new Node(key, value);
 			else
 				left->insert(key, value);
 		}
-		else
-		{
+		else {
 			if (right == nullptr)
 				right = new Node(key, value);
 			else
@@ -27,29 +26,71 @@ struct Node {
 		}
 	}
 
-	Node* search(int key)
-	{
+	Node* search(int key) {
 		if (key == this->key)
 			return this;
 		else if (key < this->key && left != nullptr)
 			left->search(key);
-		else if (key >= this->key && right != nullptr)
+		else if (key > this->key && right != nullptr)
 			right->search(key);
 		else
 			return nullptr;
 	}
 
-	Node* getMin()
-	{
+	Node* getMin() {
 		if (left != nullptr)
 			return left->getMin();
 		return this;
 	}
 
-	Node* getMax()
-	{
+	Node* getMax() {
 		if (right != nullptr)
 			return right->getMax();
 		return this;
+	}
+
+	void bypassWide() {
+		printf("%u ", this->key);
+		this->left
+	}
+
+	void bypassSymmetrical() {
+		if (this->left)
+			left->bypassSymmetrical();
+		printf("%u ", this->key);
+		if (this->right)
+			right->bypassSymmetrical();
+	}
+
+	void bypassDirect() {
+		if (this->left)
+			left->bypassDirect();
+		if (this->right)
+			right->bypassDirect();
+		printf("%u ", this->key);
+	}
+
+	void bypassReverse() {
+		printf("%u ", this->key);
+		if (this->left)
+			left->bypassReverse();
+		if (this->right)
+			right->bypassReverse();
+	}
+
+	// ?????
+	void remove(int key) {
+		if (this->left)
+			if (this->left->key == key)
+				this->left = nullptr;
+
+			else
+				this->left->remove(key);
+
+		if (this->right)
+			if (this->right->key == key)
+				this->right = nullptr;
+			else
+				this->right->remove(key);
 	}
 };
--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 
 int main(int, char*[])
 {
-	Node Root;
-	Root.insert(1, 0);
-	Root.insert(2, 0);
-	Root.insert(3, 0);
-	printf("%p\n", Root.search(0));
-	printf("%p\n", Root.search(1));
-	printf("%p\n", Root.search(2));
-	printf("%p\n", Root.search(3));
-	printf("%p\n", Root.search(4));
-	printf("________________\n");
-	printf("%i\n", Root.getMin()->key);
-	printf("%i\n", Root.getMax()->key);
+	Node* Root{ new Node(0, 0)};
+	Root->insert(1, 0);
+	Root->insert(12, 0);
+	Root->insert(12, 0);
+	Root->insert(7, 0);
+	Root->insert(8, 0);
+	Root->insert(3, 0);
+	Root->insert(4, 0);
+	printf("Bypass of binary tree:\n");
+	Root->bypassReverse();
+	printf("\n================================================================\n");
+	Root->remove(7);
+	printf("Bypass of binary tree after delete node \"7\":\n");
+	Root->bypassReverse();
+	delete Root;
 	return 0;
 }
 
